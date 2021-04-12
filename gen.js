d47be1b18e497fdac9d6d4b656244dcafc255294
reetah589 <reetah589@gmail.com>
2021-04-11 20:47:30 -0500

    updated gen.js

--- gen.js
+++ gen.js
@@ -1,5 +1,126 @@
-// Assignment code here
+// Four arrays one with special characters one with all the numbers
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
 
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+var upperCase = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+var characterNumerical = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+]
+
+var characterNumerical = "0123456789";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+
+
+// Assignment code here
+function generatePassword() {
+var passwordText = window.prompt("Enter The Text In Your Password")
+console.log(passwordText);
+console.log("Your password text is " + passwordText);
+
+var length = prompt("How long do you want your password?")
+console.log(length);
+console.log("Your password length is " + length);
+
+console.log("Your chosen text is " + passwordText + "with only a length of " + length);
+window.alert("Your chosen text is " + passwordText + "with only a length of " + length);
+
+
+var password = "";
+for(var I = 0; I<length; I++) { 
+password += getRandom ()
+} 
+return password 
+} 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +136,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// If statements check that the user gave a valid number between 8 ans 128
+// Using confirm to get a boolean value, true or false value, choose whcih type of chareacters the user wants
+
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+  return randElement;
+}
